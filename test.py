002fcdc99c1267fbc343d261955811d9340f3718
giasinguyen <giasinguyentran@gmail.com>
2020-04-19 09:55:41 +0700

    testne

---
+++ test.py
@@ -0,0 +1,7 @@
+# Có 6 học sinh điểm 5 , 7 học sinh điểm 2 , 3 học sinh điểm 10 . tính điểm trung bình
+sodiem5 = 6
+sodiem2= 7
+sodiem10 = 3
+tongdiem = sodiem5 * 6 + sodiem2 * 7 + sodiem10 * 3
+shs = sodiem10 + sodiem2 + sodiem5
+print(round(tongdiem / shs))
